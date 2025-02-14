3e5de15e2c6c2ce03b4c1e1f630792ed3ce239c4
AlexKretov <mle-user@epd58s29hlqkfg7f1nit.auto.internal>
2025-02-14 10:05:06 +0000

    dag for metros to db

--- dags/get_metros.py
+++ dags/get_metros.py
@@ -10,11 +10,36 @@ from io import StringIO
     on_success_callback=send_telegram_success_message,
     on_failure_callback=send_telegram_failure_message
 )
+
 def get_metro():
     import requests
     import pandas as pd
     import boto3
     from io import StringIO
+    @task()
+    def create_metro_table()-> None:
+        import pandas as pd
+        import numpy as np
+        import sqlalchemy
+        from sqlalchemy import MetaData, Table, Column, String, Integer, Float, DateTime,UniqueConstraint, Numeric
+        from sqlalchemy.dialects import mysql
+        # дополните импорты необходимых типов колонок
+        from sqlalchemy import inspect
+        hook = PostgresHook('destination_db')
+        conn = hook.get_sqlalchemy_engine()
+        metadata = MetaData()
+        metros_db = Table(
+            'metro_stations',
+            metadata,
+            Column('id', Numeric, primary_key=True, autoincrement=False),
+            Column('name', String),
+            Column('lat', Float),
+            Column('lng', Float),
+            Column('order', Integer),
+            Column('line', String)
+        )
+        if not inspect(conn).has_table(metros_db.name): 
+            metadata.create_all(conn)
     # 1. Загрузка файла CSV из GitHub
     @task()
     def get_csv_from_github(url):
@@ -27,35 +52,17 @@ def get_metro():
         csv_content = response.content.decode('utf-8')
         return csv_content
     @task()
-    def csv_to_s3(csv_content):
-        import pandas as pd
-        from dotenv import load_dotenv
-        from io import StringIO
-        import boto3
-        # 2. Преобразование CSV в DataFrame
-        df = pd.read_csv(StringIO(csv_content))
-        # 3. Настройка клиента AWS S3
-        load_dotenv()
-        s3_client = boto3.client(
-            's3',
-            aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-            aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY')
-            #region_name='YOUR_REGION'
-        )
-
-        # 4. Запись DataFrame в CSV в памяти
-        csv_buffer = StringIO()
-        df.to_csv(csv_buffer, index=False)
-
-        # 5. Загрузка CSV в S3
-        bucket_name = os.environ.get('S3_BUCKET_NAME')
-        s3_file_name = 'metro_stations.csv'
-
-        s3_client.put_object(
-            Bucket=bucket_name,
-            Key=s3_file_name,
-            Body=csv_buffer.getvalue()
-        )
+    def load(csv_content):
+        data = pd.read_scv(csv_content)
+        hook = PostgresHook('destination_db')
+        hook.insert_rows(
+            table="metro_stations",
+            replace=True,
+            target_fields=data.columns.tolist(),
+            replace_index=['id'],
+            rows=data.values.tolist()
+    )
+    create_metro_table()
     csv_content = get_csv_from_github("https://github.com/AlexKretov/yn/blob/main/metro_stations.csv")
-    csv_to_s3(csv_content)
+    load(csv_content)
 get_metro()
