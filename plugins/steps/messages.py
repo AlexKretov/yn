7e724cc48d3e3ef61c9b158aec9f3900890b2511
AlexKretov <mle-user@epd58s29hlqkfg7f1nit.auto.internal>
2025-02-14 15:57:58 +0000

    add plugins

--- plugins/steps/messages.py
+++ plugins/steps/messages.py
@@ -31,3 +31,26 @@ def deal_outlier(data):
     filter = (data['price'] >= Q1 - 1.5 * IQR) & (data['price']<= Q3 + 1.5 *IQR)
     train2 = data.loc[filter]  
     return train2
+
+def manhattan_distance(a, b):
+    import numpy as np
+    lat1, lon1 = a
+    lat2, lon2 = b
+    # Приблизительное преобразование градусов в километры
+    lat_km = 111  # 1 градус широты примерно равен 111 км
+    lon_km = 111 * np.cos(np.radians((lat1 + lat2) / 2))  # Учитываем сужение меридианов
+    
+    dlat = np.abs(lat2 - lat1) * lat_km
+    dlon = np.abs(lon2 - lon1) * lon_km
+    
+    return dlat + dlon
+
+def find_nearest_metro(lat, lon, all_stations):
+    import numpy as np
+    from scipy.spatial.distance import cdist
+    from tqdm import tqdm
+    metro_coords = all_stations[['lat', 'lng']].values
+    flat_coords = np.array([[lat, lon]])
+    distances = cdist(flat_coords, metro_coords, metric=manhattan_distance)[0]
+    nearest_index = np.argmin(distances)
+    return all_stations.iloc[nearest_index]['name'], distances[nearest_index]
