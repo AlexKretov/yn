b44d00434026f4e83b3a30fc02130d0f33b5ec50
AlexKretov <mle-user@epd58s29hlqkfg7f1nit.auto.internal>
2025-02-19 11:04:24 +0000

    hide all secrets

--- plugins/steps/messages.py
+++ plugins/steps/messages.py
@@ -1,25 +1,33 @@
 def send_telegram_success_message(context): # на вход принимаем словарь со контекстными переменными
     from airflow.providers.telegram.hooks.telegram import TelegramHook # импортируем хук телеграма
-    hook = TelegramHook(token='7917536946:AAFP5M6JBkvFRiCGzRbnGaebb9yhesUaYrE', chat_id='-1002302177831')
+    import os
+    from dotenv import load_dotenv
+    tg_token = os.environ.get('BOT_TOKEN')
+    tg_chat = os.environ.get('CHAT_IT')
+    hook = TelegramHook(token=tg_token, chat_id=tg_chat)
     dag = context['dag'].dag_id
     run_id = context['run_id']
     
     message = f'Исполнение DAG {dag} с id={run_id} прошло успешно!' # определение текста сообщения
     hook.send_message({
-        'chat_id': '{-1002302177831}',
+        'chat_id': tg_chat,
         'text': message
     }) # отправление сообщения 
 
 def send_telegram_failure_message(context):
     from airflow.providers.telegram.hooks.telegram import TelegramHook # импортируем хук телеграма
-    hook = TelegramHook(token='7917536946:AAFP5M6JBkvFRiCGzRbnGaebb9yhesUaYrE', chat_id='-1002302177831')
+    import os
+    from dotenv import load_dotenv
+    tg_token = os.environ.get('BOT_TOKEN')
+    tg_chat = os.environ.get('CHAT_IT')
+    hook = TelegramHook(token=tg_token, chat_id=tg_chat)
     message = "DAG failed: \nRun ID: {0}\nTask Instance Key: {1}".format(
                 #context['dag'].dag_id,
                 context['run_id'],
                 context['task_instance_key_str']
             )   
     hook.send_message({
-        'chat_id': '-1002302177831',
+        'chat_id': tg_chat,
         'text': message
     }) # отправление сообщения 
 
